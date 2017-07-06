ed2ceb21b2e748f3dea62ace70ff2532a60b39dc
Zach Williams <saumanahaii@gmail.com>
2017-07-06 14:17:12 -0400

    Working on JSDoc comments for automatic documenting in the algorithm file

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,10 @@
 //this is a temp file for experimenting with the datastructure and
 //algorithm without touching any other files.
 
-/** A deck which contains cards sorted into several decks and helper functions. */
+/** A deck which contains cards sorted into several decks and helper functions.
+* @param {object} data - The data you want to make a deck with
+* @param {object} timeFunction - An optional argument that determines how long a wait each category has before card is due
+*/
 class Deck{
   constructor(data, timeFunction){
 
@@ -18,12 +21,14 @@ class Deck{
     }
     this.name = data.name;
     this.subject = data.subject;
-    this.deck = this.makeDeck(data);
+    this.deck = this.makeDeck();
     this.populateDeck(data);
     this.stageCards(5);
   }
 
-  /** Pushes the question to the next queue as needed and toggles learning state */
+  /** Pushes the question to the next queue as needed and toggles learning state
+  * @param {object} card - Optionally the node in the queue to use.  Defaults to first card of 'now'
+  */
   answeredQuestionCorrectly(card = this.fetchFirst("now")){
     if(card.study){
       card.study = false;
@@ -44,14 +49,18 @@ class Deck{
     }
   }
 
-  answeredQuestionIncorrectly(card = this.fetchFirst("now")){
+  /** Moves card to end of stack and resets the currentStack*/
+  answeredQuestionIncorrectly(){
     //move the card to the end of the queue
     let failedCard = this.shift("now");
     failedCard.currentStack = "now";
     this.fetchEnd("now").child = failedCard;
-
   }
 
+  /** Check a stack for whether the face card's due time is less than now
+  * @param {string} target - The queue's name that is being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   checkStackforDueCards(target, deck = this.deck){
     if(deck[target]){
       if(this.fetchFirst(target).due < Date.now()){
@@ -61,19 +70,40 @@ class Deck{
     }
   }
 
+  /** calls the checkStackforDueCards function on all stacks
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
+  checkAllStackforDueCards(deck = this.deck){
+    this.checkStackforDueCards("soon", deck);
+    this.checkStackforDueCards("soonish", deck);
+    this.checkStackforDueCards("later", deck);
+    this.checkStackforDueCards("learned", deck);
+  }
+
+  /** populates the deck from the provided json object
+  * @param {object} data - The data with which to populate the deck as an object.
+  */
   populateDeck(data){
     data.deck.forEach(el => {
       this.add(el, "unstaged");
     })
   }
 
-  /** Adds a card to the target deck.  Calls makeCard to turn data into a new card. */
+  /** Adds a card to the target deck.  Calls makeCard to turn data into a new card.
+  * @param {object} data - The data to make a card with
+  * @param {string} target - The sting name of the targeted queue
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   add(data, target, deck = this.deck){
     let card = this.makeCard(data);
     this.push(card, target, deck);
 
   }
-  /** Takes a list and pops an element off the end.*/
+
+  /** Takes a list and pops an element off the end.
+  * @param {string} target - The string name of the queue being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   pop(target, deck = this.deck){
     let item = deck[target];
     item.parent.child = null;
@@ -81,10 +111,12 @@ class Deck{
     return item;
   }
 
-  /** Takes a list and removes the first item from it. */
+  /** Takes a list and removes the first item from it.
+  * @param {string} target - The string name of the queue being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   shift(target, deck = this.deck){
     let item = this.fetchFirst(target, deck);
-    //console.log(item)
     if(deck[target].child){
         deck[target] = deck[target].child;
         deck[target].parent = null;
@@ -96,7 +128,11 @@ class Deck{
     return item;
   }
 
-  /** push item onto end of queue */
+  /** push item onto end of queue
+  *@param {object} card - The card to push onto the queue, as formatted by the makeCard function
+  * @param {string} target - The string name of the queue being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   push(card, target, deck = this.deck){
     let head = this.fetchEnd(target);
     //if first element does not exist yet, make it
@@ -108,11 +144,18 @@ class Deck{
     }
   }
 
-  /** Returns the first element of a queue without removing it. */
+  /** Returns the first element of a queue without removing it.
+  * @param {string} target - The string name of the queue being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   fetchFirst(target, deck = this.deck){
     return deck[target];
   }
-  /** Returns the last element of a queue without removing it. */
+
+  /** Returns the last element of a queue without removing it.
+  * @param {string} target - The string name of the queue being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   fetchEnd(target, deck = this.deck){
     //fetches the end of the queue, so that we can stick on an element.
     if(deck[target]){
@@ -125,6 +168,10 @@ class Deck{
     return null;
   }
 
+  /** Move a number of cards from unstaged to now
+  * @param {integer} num - Optionally the number of cards to move from 'unstaged' to 'now'.  Defaults to 1.
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   stageCards(num = 1, deck = this.deck){
     for(let i=0;i<num;i++){
       this.switchStack("unstaged", "now", deck);
@@ -132,6 +179,11 @@ class Deck{
     this.deck.counts["now"] += num;
   }
 
+  /** Move face card from one stack to the end of another and manage count
+  * @param {string} origin - The string name of the origin queue
+  * @param {string} target - The string name of the queue being targeted
+  * @param {object} deck - Optional argument to target a specific deck.  Defaults to current deck.
+  */
   switchStack(origin, target,deck=this.deck){
     let item = this.shift(origin, deck);
     if(origin != "unstaged"){
@@ -143,6 +195,10 @@ class Deck{
   }
 
   // NOTE: I've toggled study to false so that I can test the code.
+  /** Makes a card given the cardData.  If no parent passed in, it is assumed to be root node
+  * @param {object} cardData - The data with which to make a card
+  * @param {object} parent - The parent card.  Defaults to null, which means that the queue is empty.  You should almost always call it with a parent argument.
+  */
   makeCard(cardData,parent = null){
     cardData.parent = parent;
     cardData.child = null;
@@ -152,7 +208,8 @@ class Deck{
     return cardData;
   }
 
-  makeDeck(data){
+  /** Creates an empty deck object ready to be populated.*/
+  makeDeck(){
     //makes a tree/queus structure
     return {
       counts: {
